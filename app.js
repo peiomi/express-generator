adeb7c5a614d1fcc3287b2ea1f05d54c53501973
paydon <paydonriley@gmail.com>
2026-07-06 13:02:49 -0500

    Basic Authentication

--- app.js
+++ app.js
@@ -54,4 +54,28 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// authentication
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    const err = new Error("You are not authenticated!");
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+  const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === "admin" && pass === "password") {
+    return next();
+  } else {
+    const err = new Error("You are not authenticated!");
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+app.use(auth);
+
 module.exports = app;
