7d46456fa1727147401ff93c1dda967288eb7b18
paydon <paydonriley@gmail.com>
2026-07-08 11:14:32 -0500

    Express Session Pt2

--- app.js
+++ app.js
@@ -43,34 +43,20 @@ app.use(session({
   store: new FileStore()
 }));
 
-// authentication
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// authentication need to come b4 routers
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-
-    const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === "admin" && pass === "password") {
-      req.session.user = "admin";
-      return next();
-    } else {
-      const err = new Error("You are not authenticated!");
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
+    const err = new Error("You are not authenticated!");
+    err.status = 401;
+    return next(err);
 
   } else {
-    if (req.session.user === "admin") {
+    if (req.session.user === "authenticated") {
       return next();
     } else {
       const err = new Error("You are not authenticated!");
@@ -81,9 +67,7 @@ function auth(req, res, next) {
 }
 app.use(auth);
 
-// routers need to be mounted after express parses json 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// routers need to be mounted after express parses json && b4 404
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
