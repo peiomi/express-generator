c12e606e516e1a8fb961795d2844cb85895c68fb
paydon <paydonriley@gmail.com>
2026-07-08 10:33:06 -0500

    cookies

--- app.js
+++ app.js
@@ -54,26 +54,39 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.use(cookieParser("12345-67890-09876-54321"));
+
 // authentication
 function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    const err = new Error("You are not authenticated!");
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
-  }
-  const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
-  const user = auth[0];
-  const pass = auth[1];
-  if (user === "admin" && pass === "password") {
-    return next();
+  if (!req.signedCookies.user) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      const err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
+
+    const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+    const user = auth[0];
+    const pass = auth[1];
+    if (user === "admin" && pass === "password") {
+      return next();
+    } else {
+      const err = new Error("You are not authenticated!");
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+
   } else {
-    const err = new Error("You are not authenticated!");
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+    if (req.signedCookies.user === "admin") {
+      return next();
+    } else {
+      const err = new Error("You are not authenticated!");
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 app.use(auth);
