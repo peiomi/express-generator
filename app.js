42c2f77844c42735fdc75f15b395d8b144b447f5
paydon <paydonriley@gmail.com>
2026-07-09 15:05:50 -0500

    passport

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const logger = require('morgan');
 const mongoose = require("mongoose");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const authenticate = require("./authenticate");
 
 const url = 'mongodb://127.0.0.1:27017/nucampsite';
 const connect = mongoose.connect(url, {});
@@ -42,6 +44,9 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
+// need to be after session
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -50,19 +55,13 @@ app.use('/users', usersRouter);
 function auth(req, res, next) {
   console.log(req.session);
 
-  if (!req.session.user) {
+  if (!req.user) {
     const err = new Error("You are not authenticated!");
     err.status = 401;
     return next(err);
 
   } else {
-    if (req.session.user === "authenticated") {
-      return next();
-    } else {
-      const err = new Error("You are not authenticated!");
-      err.status = 401;
-      return next(err);
-    }
+    return next();
   }
 }
 app.use(auth);
